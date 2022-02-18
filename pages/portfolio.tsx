8a154715d8d103dd577548734137dc1c18839ad3
Zeno3463 <zenolim3463@gmail.com>
2022-02-19 07:36:13 +0800

    feat: Added zeno-blog to portfolio

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -100,6 +100,7 @@ export default function portfolio() {
 			<div className={style.portfolioContainer}>
 				<PortfolioProjectTemplate title="Whiteboard.fi Clone" description="a multi-user whiteboard sharing platform built using next.js, express.js, and socket.io" githubRepo="https://github.com/Zeno3463/whiteboard.fi-clone" />
 				<PortfolioProjectTemplate title="I Can Do It" description="a website/API that generates random motivational content" githubRepo="https://github.com/Zeno3463/i-can-do-it" url="https://i-can-do-it.vercel.app/" />
+				<PortfolioProjectTemplate title="Zeno Blog" description="a personal blog website built using next.js, mongodb, and tailwindcss" githubRepo="https://github.com/Zeno3463/zeno-blog" url="https://zeno-blog.vercel.app/" />
 			</div>
 			<h2>My Other Projects</h2>
 			<div className={style.portfolioContainer}>
